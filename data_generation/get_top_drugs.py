9fe960c6f42440215ea7847e74e32d5e4a21481d
Nechus0 <joscha.pocha@gmx.de>
2026-07-21 00:25:43 +0200

    Update

--- data_generation/get_top_drugs.py
+++
@@ -1,6 +0,0 @@
-import urllib.request
-import json
-import re
-
-url = "https://gesund.bund.de/suchen/medikamente"
-print("Cannot easily scrape top 500 without an API. I will use a known static list of top active ingredients in Germany.")
